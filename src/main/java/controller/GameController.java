3db181c5f688bdf235c78696e1363f2964d136f2
romain <ramageromain@gmail.com>
2015-10-16 12:38:32 +0200

    test display in grid with controller

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import model.Board;
 import model.BoardImpl;
+import model.Tile;
 
 import java.io.IOException;
 import java.net.URL;
@@ -26,6 +27,21 @@ public class GameController implements Initializable {
 
     public void initialize(URL location, ResourceBundle resources) {
         boardModel = new BoardImpl(10);
-        grid.getChildren().add(0,new Label("test"));
+        grid.setGridLinesVisible(true);
+        for(int i=1;i<=10;i++){
+            for(int j=1;j<=10;j++) {
+                Tile t = boardModel.getTile(i,j);
+
+                if(t!=null){
+                    System.out.println("pas null value i=" +i +" j="+j);
+                    String value = String.valueOf(t.getRank());
+
+                    grid.add(new Label(value), i - 1, j - 1);
+                }else{
+                    grid.add(new Label(String.valueOf(i)), i - 1, j - 1);
+                    System.out.println("null value i=" +i +" j="+j);
+                }
+            }
+        }
     }
 }
